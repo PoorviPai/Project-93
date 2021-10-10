37fef8abbe4df8ce1a84e409a883911ea90db7e2
PoorviPai <83392771+PoorviPai@users.noreply.github.com>
2021-10-10 12:43:14 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,28 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBKBKKkck_tZCav1ygkOc1xRgcpBY_YEiY",
+    authDomain: "kwitter-1-7f21c.firebaseapp.com",
+    databaseURL: "https://kwitter-1-7f21c-default-rtdb.firebaseio.com",
+    projectId: "kwitter-1-7f21c",
+    storageBucket: "kwitter-1-7f21c.appspot.com",
+    messagingSenderId: "735510015144",
+    appId: "1:735510015144:web:83102c3e6362846499a69d"
+  };
+  
+  // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+
+    user_name = localStorage.getItem("Username");
+    document.getElementById("welcome_user_name").innerHTML = "Welcome "+user_name+"!";
+
+    function addroom() {
+           room_name = document.getElementById("room_name").value;
+  
+          localStorage.setItem("Roomname",room_name);
+      
+          window.location = "kwitter_page.html";
+    }
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+     Room_names = childKey;
+    });});}
+getData();
